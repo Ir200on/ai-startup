dbc90e14867194da637b86e107f8c3aa364a6b84
Ir200on <snezokdasha@gmail.com>
2025-04-25 16:32:55 +0300

    rename all files after rename folder

--- frontend-tribe-ai-startup/next.config.mjs
+++ frontend-tribe-ai-startup/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/frontend-tribe-ai-startup-landing-page-template',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/frontend-tribe-ai-startup-landing-page-template/' : '',
+  basePath: '/frontend-tribe-ai-startup',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/frontend-tribe-ai-startup/' : '',
   images: {
     unoptimized: true,
   },
